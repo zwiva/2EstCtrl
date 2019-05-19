aa99b119c2c2e69693f010594753e5bf0ee440c0
Andrea Castro <ap.castrocastellon@gmail.com>
2019-05-19 00:44:33 -0400

    ejericios 6 y 7

---
+++ 6ec.rb
@@ -0,0 +1,13 @@
+#1.6 Utiliza álgebra booleana para hacer un refactoring de este código. Para verificar la evaluación de condiciones puedes modificar los valores de a y b.
+
+a = 'verdadero'
+b = 'falso'
+
+if a == 'verdadero' && b == 'verdadero'
+    puts ':)'
+elsif a == 'falso' && b == 'verdadero'
+    puts ':|'
+else 
+    puts ':('
+    
+end
