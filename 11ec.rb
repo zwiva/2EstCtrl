5807f8597ffed42e561e79e82777ba6349a6fd30
Andrea Castro <ap.castrocastellon@gmail.com>
2019-05-19 01:13:37 -0400

    ejercicio 11

---
+++ 11ec.rb
@@ -0,0 +1,10 @@
+# El siguiente código busca sumar todos los números del 1 al 10, pero no funciona porque algo falta ¿puedes arreglarlo?
+# Hint: El total debería sumar 55.
+
+suma = 0 #fattaba agregar acumulador
+
+10.times do |i|
+    suma += i + 1
+end
+
+puts suma
